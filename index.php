69ec100c81739626b840958a397df6ea115ca888
RobWillison <rob.willison@mayden.co.uk>
2016-01-09 16:57:23 +0000

    f

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+require_once __DIR__ . '/SpeakServer/Objects/SpeakObject.php';
+require_once __DIR__ . '/SpeakServer/Service/Speak.php';
+
 use SpeakServer\Service\Speak;
 use SpeakServer\Objects\SpeakObject;
 
