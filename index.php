a83ebc0af4da1cee48b3880a3b3510dc38353a1f
RobWillison <rob.willison@mayden.co.uk>
2016-01-09 16:50:37 +0000

    f

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 use SpeakServer\Service\Speak;
 use SpeakServer\Objects\SpeakObject;
 
+var_dump(get_included_files());
+
 if (isset($_POST['text'])) {
     $toSpeak = new SpeakObject();
     $toSpeak->addText($_POST['text']);
