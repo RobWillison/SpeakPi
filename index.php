591955897146ed422bed3cfb61e3830f3aad4256
RobWillison <rob.willison@mayden.co.uk>
2016-01-09 21:49:04 +0000

    all

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (isset($_POST['text'])) {
     $speaker = new Speak();
     $speaker->speak($toSpeak);
 } elseif (isset($_POST['addbutton'])) {
-    $fp = fopen('Button.txt', 'a+');
+    $fp = fopen('Buttons.txt', 'a+');
     fwrite($fp, $_POST['addbutton']);
     fclose($fp);
 } else {
