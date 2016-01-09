ea0b9c322f44e54f4713fc184db0e07bb049d45f
RobWillison <rob.willison@mayden.co.uk>
2016-01-09 17:54:01 +0000

    f

--- SpeakServer/Service/Speak.php
+++ SpeakServer/Service/Speak.php
@@ -10,7 +10,6 @@ class Speak
     public function speak(SpeakObject $object)
     {
         $text = addslashes($object->getText());
-        flock(__DIR__ . '/../../lock', LOCK_EX, 1);
         exec("espeak -ven+f3 -k5 -s150 '$text'");
     }
 
