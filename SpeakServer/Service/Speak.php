22878968de2a1f9b329f0d4633d23e501a6cad2e
RobWillison <rob.willison@mayden.co.uk>
2016-01-09 18:00:40 +0000

    f

--- SpeakServer/Service/Speak.php
+++ SpeakServer/Service/Speak.php
@@ -10,8 +10,10 @@ class Speak
     public function speak(SpeakObject $object)
     {
         $text = addslashes($object->getText());
-        $fp = fopen(__DIR__ . '. /../../lock', "w");
+
+        $fp = fopen(__DIR__ . '/../../lock', "w");
         flock($fp, LOCK_EX);
+
         exec("espeak -ven+f3 -k5 -s150 '$text'");
     }
 
