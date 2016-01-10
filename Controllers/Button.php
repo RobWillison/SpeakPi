40979f8fe9f9b0ff5bb7cef08cfd211c56c65b44
RobWillison <rob.willison@mayden.co.uk>
2016-01-10 16:38:30 +0000

    hello

--- Controllers/Button.php
+++ Controllers/Button.php
@@ -18,7 +18,7 @@ class Button implements GenericController
     public function put()
     {
         parse_str(file_get_contents("php://input"),$post_vars);
-        var_dump($post_vars);
+        var_dump($_GET);
         if(isset($post_vars['addbutton'])) {
             $this->addButton();
         }
