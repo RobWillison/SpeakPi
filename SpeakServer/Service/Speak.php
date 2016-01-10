5674d9af5f9e784d055a2cb28e078f4619a89167
RobWillison <rob.willison@mayden.co.uk>
2016-01-10 17:09:39 +0000

    hello

--- SpeakServer/Service/Speak.php
+++ SpeakServer/Service/Speak.php
@@ -13,10 +13,11 @@ class Speak
 
         $voice = $object->getVoice();
         $from = $object->getFrom();
+        $gender = $object->getGenderExtension();
 
         $text = $from == null ? $text : $from . ' Says ' . $text;
 
-        exec("espeak -k5 -s150 '$text' -v $voice");
+        exec("espeak -k5 -s150 '$text' -v $voice$gender");
     }
 
 }
