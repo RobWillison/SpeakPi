10095e5302e2e7099841df0f150aa60999b7130e
RobWillison <rob.willison@mayden.co.uk>
2016-01-10 14:53:21 +0000

    hello

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $config = [
 
 $controllerName = $config['page'][$_GET['page']];
 $methodName = $_SERVER['REQUEST_METHOD'];
-
+var_dump($controllerName);die;
 $controller = new $controllerName;
 var_dump($controller);die;
 $controller->get();
